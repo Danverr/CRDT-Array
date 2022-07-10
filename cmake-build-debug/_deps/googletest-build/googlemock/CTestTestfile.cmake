96d80012693e1b14f295488c8c1fed917eab2b2b
Danverr <79198736000@mail.ru>
2022-07-10 23:44:27 +0300

    Gitignore init

--- cmake-build-debug/_deps/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/d/Projects/Contests and Interviews/Bricks/CRDT Hometask/cmake-build-debug/_deps/googletest-src/googlemock
-# Build directory: /mnt/d/Projects/Contests and Interviews/Bricks/CRDT Hometask/cmake-build-debug/_deps/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
